d2c82f3237885476b75c868081a7420467e98993
Alexey <ipad2322012@gmail.com>
2023-12-06 13:18:39 +0300

    дз 5-го семинара

---
+++ task 1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = { // Создаем двумерный массив (матрицу)
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        Console.Write("Введите номер строки: ");// Запрашиваем у пользователя позиции элемента
+        int row = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите номер столбца: ");
+        int column = int.Parse(Console.ReadLine());
+
+        int result = GetMatrixElement(matrix, row, column);// Получаем и выводим значение элемента или сообщение об отсутствии элемента
+        if (result != int.MinValue)
+        {
+            Console.WriteLine($"Значение элемента на позиции ({row}, {column}): {result}");
+        }
+        else
+        {
+            Console.WriteLine($"Элемента на позиции ({row}, {column}) не существует.");
+        }
+    }
+    static int GetMatrixElement(int[,] matrix, int row, int column) // Функция для получения значения элемента в двумерном массиве
+    {
+        int rowCount = matrix.GetLength(0);
+        int columnCount = matrix.GetLength(1);
+
+        if (row >= 0 && row < rowCount && column >= 0 && column < columnCount) // Проверяем, существуют ли указанные позиции в массиве
+        {
+            return matrix[row, column];// Возвращаем значение элемента
+        }
+        else
+        {
+            return int.MinValue; // Возвращаем специальное значение, указывающее, что элемента нет
+        }
+    }
+}
---
+++ task 2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = new int[,] { { 1, 2, 3 }, // Создаем двумерный массив
+                                    { 4, 5, 6 },
+                                    { 7, 8, 9 } };
+
+        int rows = array.GetLength(0); // Получаем количество строк и столбцов
+        int cols = array.GetLength(1);
+
+        for (int i = 0; i < cols; i++) // Меняем местами первую и последнюю строки
+        {
+            int temp = array[0, i];
+            array[0, i] = array[rows - 1, i];
+            array[rows - 1, i] = temp;
+        }
+
+        for (int i = 0; i < rows; i++) // Выводим измененный массив
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
